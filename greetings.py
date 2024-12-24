e3d618a3a68090fe4c590f8c708a3e879102a7eb
NPC-maker <sencikn@gmail.com>
2024-12-24 23:55:26 +0400

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,13 @@
+# Grade 1 Этап 1 Задание 1
+username="Арсений"
+title="Первая запись"
+content="Описание заметки"
+status="в работе"
+created_date="10-11-2024"
+issue_date="10-12-2024"
+print("Имя:",username)
+print("Название заметки:",title)
+print("Описание заметки:",content)
+print("Статус выполнения:",status)
+print("Дата создания заметки:",created_date)
+print("Дедлайн:",issue_date)
