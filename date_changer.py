b214c5116044f7d80d3f44deba7922dd95bbb070
NPC-maker <sencikn@gmail.com>
2024-12-25 00:32:54 +0400

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,14 @@
+# Grade 1 Этап 1 Задание 2
+username="Арсений"
+title="Первая запись"
+content="Описание заметки"
+status="в работе"
+created_date="10-11-2024"
+issue_date="10-12-2024"
+print("Имя:",username)
+print("Название заметки:",title)
+print("Описание заметки:",content)
+print("Статус выполнения:",status)
+print("Дата создания заметки:",created_date[0:5])
+print("Дедлайн:",issue_date[0:5])
+
