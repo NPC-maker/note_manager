c1b55c2888b23e9ae873274f6124a5e65f768471
NPC-maker <sencikn@gmail.com>
2024-12-25 20:10:09 +0400

    Add files via upload

---
+++ add_input.py
@@ -0,0 +1,13 @@
+# Grade 1 Этап 1 Задание 3
+username=input("Введите имя: ")
+title=input("Название заметки: ")
+content=input("Описание заметки: ")
+status=input("Статус выполнения: ")
+created_date=input("Дата создания: ")
+issue_date=input("Дедлайн: ")
+print("Имя:",username)
+print("Название заметки:",title)
+print("Описание заметки:",content)
+print("Статус выполнения:",status)
+print("Дата создания заметки:",created_date[0:5])
+print("Дедлайн:",issue_date[0:5])
